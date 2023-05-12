7c6f6014889d062be14cc2e19e4f254ac60e9526
Eugene Fedoriv <57968389+eugene-f02@users.noreply.github.com>
2023-05-12 14:24:56 -0400

    fixed workflow (#82)
    * fixed workflow
    
    * bumped to 1.7.17

--- editormd.js
+++ editormd.js
@@ -2,12 +2,12 @@
  * Editor.md
  *
  * @file        editormd.js 
- * @version     v1.7.16 
+ * @version     v1.7.17 
  * @description Open source online markdown editor.
  * @license     MIT License
- * @author      Pandao
- * {@link       https://github.com/pandao/editor.md}
- * @updateTime  2023-05-11
+ * @author      IBM Skills Network
+ * {@link       https://github.com/ibm-skills-network/editor.md}
+ * @updateTime  2023-05-12
  */
 
 ;(function(factory) {
@@ -59,7 +59,7 @@
     };
 
     editormd.title        = editormd.$name = "Editor.md";
-    editormd.version      = "1.7.16";
+    editormd.version      = "1.7.17";
     editormd.homePage     = "https://pandao.github.io/editor.md/";
     editormd.classPrefix  = "editormd-";
 
